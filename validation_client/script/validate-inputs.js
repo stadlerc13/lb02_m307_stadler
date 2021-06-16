b89e01caabb097043581c900c231f31b31662e3c
stadlerc13 <chantal.stadler@gmx.ch>
2021-06-16 22:50:58 +0200

    dritter Commit, CSS ergänzt

--- validation_client/script/validate-inputs.js
+++ validation_client/script/validate-inputs.js
@@ -4,16 +4,24 @@
 */
 
 const form = document.getElementById('form');
+ const name = document.getElementById('name');
+const lastname = document.getElementById('lastname');
 const username = document.getElementById('username');
 const email = document.getElementById('email');
+const phone = document.getElementById('phone');
 const password = document.getElementById('password');
+const birthdate = document.getElementById('birthdate');
+const c1 = document.getElementById('c1');
+const c2 = document.getElementById('c2');
+const c3 = document.getElementById('c3');
+const c4 = document.getElementById('c4');
 
 // Show input error message
 function showError(input, message) {
-  const formControl = input.parentElement;
-  formControl.className = 'form-control error';
-  const small = formControl.querySelector('small');
-  small.innerText = message;
+    const formControl = input.parentElement;
+    formControl.className = 'form-control error';
+    const small = formControl.querySelector('small');
+    small.innerText = message;
 }
 
 // Show success outline
@@ -38,6 +46,15 @@ function checkEmail(input) {
     im Internet nach "javascript regular expression for mobile number".
 */
 // Check phone is valid
+  function checkPhone(input) {
+  const re = /^(+41|0041|0){1}((0))?[0-9]{9}$/;
+  if (re.test(input.value.trim())) {
+    showSuccess(input);
+  } else {
+    showError(input, 'Mobilenummer ist nicht richtig');
+  }
+}
+
 
 
 // Check required fields
@@ -85,16 +102,20 @@ function getFieldName(input) {
 }
 
 function validateForm(){
-  if(!checkRequired([username, email, password])){
+  if(!checkRequired([name, lastname, username, email, phone, birthdate, password])){
     //Aufgabe: Validierung der Länge für Vorname (2 bis 20) und Nachname (2 bis 50)
+    checkLength(name, 2, 15);
+    checkLength(lastname, 2, 15);
     checkLength(username, 3, 15);
     checkLength(password, 6, 25);
     /* Aufgabe:
       Validierung der Telefonnumer ähnlich wie bei der Email mit einer
       Regular expression (regex). Für eine geeignete regex suchen Sie
       im Internet nach "javascript regular expression for mobile number"
+      name, lastname, username, email, phone, birthdate, password
     * */
     checkEmail(email);
+
     /* Aufgabe:
       Validierung Sie die beiden Passwörter, damit password
       mit password2 übereinstimmt.
